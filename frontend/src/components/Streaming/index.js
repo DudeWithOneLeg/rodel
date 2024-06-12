0da0b1e5c67401b8ac391d8bb2e6abfc021e99d1
dudewithoneleg <galvancromeo@gmail.com>
2024-06-12 00:56:06 -0500

    socket changes

--- frontend/src/components/Streaming/index.js
+++ frontend/src/components/Streaming/index.js
@@ -1,67 +1,67 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Peer from 'peerjs';
+import React, { useRef, useEffect, useState } from "react";
+import Peer from "peerjs";
 
+const Streaming = ({ socket }) => {
+  const [peerId, setPeerId] = useState(null);
+  const [conn, setConn] = useState(null);
+  const videoRef = useRef(null);
 
+  useEffect(() => {
+    const peer = new Peer("react-receiver");
 
-const Streaming = ({socket}) => {
-    const [peerId, setPeerId] = useState(null);
-    const [conn, setConn] = useState(null);
-    const videoRef = useRef(null);
+    peer.on("open", (id) => {
+      setPeerId(id);
+      console.log("My peer ID is:", id);
+      socket.emit("register", id);
+    });
 
+    peer.on("connection", (connection) => {
+      setConn(connection);
+      connection.on("data", (data) => {
+        console.log("Received:", data);
+      });
+    });
 
-    useEffect(() => {
-        const peer = new Peer('react-receiver');
+    peer.on("call", (call) => {
+      call.answer();
+      call.on("stream", (remoteStream) => {
+        videoRef.current.srcObject = remoteStream;
+      });
+    });
 
-        peer.on('open', (id) => {
-            setPeerId(id);
-            console.log('My peer ID is:', id);
-            socket.emit('register', id);
-        });
-
-        peer.on('connection', (connection) => {
-            setConn(connection);
-            connection.on('data', (data) => {
-                console.log('Received:', data);
-            });
-        });
-
-        peer.on('call', (call) => {
-            call.answer();
-            call.on('stream', (remoteStream) => {
-                videoRef.current.srcObject = remoteStream;
-            });
-        });
-
-        if (socket) {
-            socket.on('signal', async ({ peerId: senderId, payload }) => {
-            if (payload.type === 'offer') {
-                const call = peer.call(senderId, new MediaStream());
-                call.on('stream', (remoteStream) => {
-                    videoRef.current.srcObject = remoteStream;
-                });
-                await peer._connections[senderId][0].peerConnection.setRemoteDescription(payload);
-            } else if (payload.type === 'ice') {
-                await peer._connections[senderId][0].peerConnection.addIceCandidate(payload);
-            }
-        });
+    if (socket) {
+      socket.on("signal", async ({ peerId: senderId, payload }) => {
+        if (payload.type === "offer") {
+          const call = peer.call(senderId, new MediaStream());
+          call.on("stream", (remoteStream) => {
+            videoRef.current.srcObject = remoteStream;
+          });
+          console.log(peer._connections)
+          if (peer._connections[senderId] && peer._connections[senderId][0]) {
+            await peer._connections[senderId][0].peerConnection.setRemoteDescription(payload);
         }
 
+        } else if (payload.type === "ice") {
+          await peer._connections[senderId][0].peerConnection.addIceCandidate(
+            payload
+          );
+        }
+      });
+    }
 
+    return () => {
+      peer.destroy();
+      socket.close();
+    };
+  }, [socket]);
 
-        return () => {
-            peer.destroy();
-            socket.close();
-        };
-    }, [socket]);
-
-    return (
-        <div>
-
-            <div>
-                <video ref={videoRef} autoPlay muted style={{ width: '300px'}}></video>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <video ref={videoRef} autoPlay muted style={{ width: "300px" }}></video>
+      </div>
+    </div>
+  );
 };
 
 export default Streaming;
